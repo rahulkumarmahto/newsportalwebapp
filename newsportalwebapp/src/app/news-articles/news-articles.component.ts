b0038fcb9075521fb5988925320bc561051548e2
Rahul Kumar Mahto <rahulkumarmahto@hotmail.com>
2024-05-26 19:21:40 +0530

    corrected matchingRecordCount and its page numbers- cleanups

--- newsportalwebapp/src/app/news-articles/news-articles.component.ts
+++ newsportalwebapp/src/app/news-articles/news-articles.component.ts
@@ -19,42 +19,40 @@ import { MessageService } from 'primeng/api';
 })
 export class NewsArticlesComponent implements OnInit {
 
-
   newsArticles: any[] = [];
-  cols: any[] = [];
   loading: boolean = true;
-  rowsToDisplay?: number;
+  rowsToDisplay: number = 5;
   totalCount: number = 0;
-  itemsCount?: number;
-  matchingRecordCount?: number;
+  itemsCount: number = 0;
+  matchingRecordCount: number = 0;
 
   constructor(
     private router: Router,
     private messageService: MessageService,
     private newsArticleService: NewsArticleService) {
-
   }
+
   ngOnInit(): void {
     this.loading = true;
   }
+
   loadNewsArticles(event: any) {
     this.loading = true;
     let searchText = '';
     let pageIndex = 1;
-
-    console.log(event);
-    if (event.first)
-      pageIndex = event.first == 0 ? 1 : (event.first / event.rows) + 1;
-
     let sortBy = 'CreatedDateTime';
     let sortDirection = "DESC";
     let pageSize = event.rows;
     this.rowsToDisplay = event.rows ?? 5;
 
+    if (event.first)
+      pageIndex = event.first == 0 ? 1 : (event.first / event.rows) + 1;
+
     if (event.globalFilter || event.sortField) {
       sortBy = event.sortField ?? 'CreatedDateTime';
       sortDirection = event.sortOrder == 1 ? "ASC" : "DESC";
       pageSize = event.rows;
+
       if (event.filters.global) {
         searchText = event.filters.global.value
       }
@@ -72,12 +70,7 @@ export class NewsArticlesComponent implements OnInit {
       this.newsArticleService.getByQueryParameters(queryParameters).subscribe(res => {
         this.newsArticles = res.items ?? [];
         this.totalCount = res.totalCount;
-        if (event.globalFilter || event.sortField) {
-          this.matchingRecordCount = res.itemsCount;
-        } else {
-          this.matchingRecordCount = res.totalCount;
-        }
-
+        this.matchingRecordCount = res.matchingRecordCount;
         this.loading = false;
       },
         error => {
@@ -87,14 +80,10 @@ export class NewsArticlesComponent implements OnInit {
 
   }
 
-  inputTextSearch(dt: any, event: any) {
-    dt.filterGlobal(event.target.value, 'contains');
-  }
-
   addNewNewsArticle() {
     this.router.navigateByUrl("/NewsArticle/Manage/0");
-
   }
+
   editNewsArticle(request: any) {
     this.router.navigateByUrl("/NewsArticle/Manage/" + request.id);
   }
@@ -102,5 +91,4 @@ export class NewsArticlesComponent implements OnInit {
   deleteNewsArticle(request: any) {
     this.router.navigateByUrl("/NewsArticle/Delete/" + request.id);
   }
-
 }
